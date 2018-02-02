3c1c68b045cc2173e0da265feb3e5559f7ab03d5
alvin.svitzer <alsvitzer@gmail.com>
2018-02-02 10:49:09 +0100

    Adding recyclerview layout and adapter for forecast

---
+++ app/src/main/java/com/alvinsvitzer/weathervane/activities/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.alvinsvitzer.weathervane.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.alvinsvitzer.weathervane.R
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_recycler_view)
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+    }
+}
